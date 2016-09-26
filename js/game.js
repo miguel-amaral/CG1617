cc323188ff8802284b3a861f3bdff14ee917dee9
Miguel Antao Pereira Amaral <miguel.p.amaral@tecnico.ulisboa.pt>
2016-09-26 19:46:04 +0100

    missing borderline in ship movement; otherwise ship is good

--- js/game.js
+++ js/game.js
@@ -5,20 +5,22 @@ var nave3;
 
 var shipSpeed=0;
 var shipAcceleration=0;
+var clk;
 
 function init(){
 	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
 	camera.position.set(20,100,100);
+	createScene();
 	renderer = new THREE.WebGLRenderer();
   	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild( renderer.domElement ) ;
 
-	createScene();
+	clk = new THREE.Clock();
 	camera.lookAt(new THREE.Vector3(20,0,0));
 
-	window.addEventListener("keydown", checkKey);
+	window.addEventListener("keydown", onKeyDown);
 	window.addEventListener("keyup", onKeyUp);
-	
+
 	animate();
 }
 
@@ -31,52 +33,58 @@ function createScene(){
 	var j = 0;
 	while(j < 2){
 		var i = 0;
-		while(i < 8){
+		while(i < 4){
 			var enemy1 = new Enemy(scene,i*5,0,j*5);
 			i++;
 		}
 		j++;
 	}
-	scene.add(new THREE.AxisHelper(1));
+	scene.add(new THREE.AxisHelper(10));
 
 }
 
 function animate(){
-
-	shipSpeed+=shipAcceleration;
-	
-
-	nave3.setPosition(nave3.x+shipSpeed, nave3.y, nave3.z);
+    var dt = clk.getDelta();
+	nave3.updatePosition(dt);
 
 	renderer.render(scene, camera);
 	requestAnimationFrame(animate);
-	
-
 }
 
-function checkKey (event) {
-	
+function onKeyDown (event) {
+
 	switch (event.keyCode) {
 
-		case 37: 
-			//alert ("left key");
-			shipAcceleration=-0.01;
+		case 37://alert ("left key");
+			nave3.startLeft(clk.getElapsedTime());
 			break;
-		case 38:
-			//alert("up key");
+		case 38://alert("up key");
 			break;
-		case 39: 
-			//alert ("right key");
-			shipAcceleration=0.01;
+		case 39://alert ("right key");
+			nave3.startRight(clk.getElapsedTime());
 			break;
-		case 40: 
-			//alert ("down key");
+		case 40://alert ("down key");
 			break;
-	}	
+		case 97: //a
+		case 65: //A
+			//TODO
+			break;
+	}
 
 }
 
 function onKeyUp (event) {
-	shipAcceleration=0;
-	shipSpeed=0;
+	switch (event.keyCode) {
+		case 37://alert ("left key");
+			nave3.stopLeft();
+			break;
+		case 38://alert("up key");
+			break;
+		case 39://alert ("right key");
+			nave3.stopRight();
+			break;
+		case 40://alert ("down key");
+			break;
+
+		}
 }
--- js/ship.js
+++ js/ship.js
@@ -1,9 +1,15 @@
+const TOP_SPEED   = 50;
+const DEBUG       = 0;
+const MIN_SPEED   = 1.3;
+const ACELARATION = Math.pow(TOP_SPEED,2);
 class Ship {
 	constructor(scene,x,y,z){
 		this.ship = new THREE.Object3D();
-		this.velocity = 0;
+
+		this.speed = 0;
 
 		this.setPosition(x,y,z);
+		this.updatePosition(0);
 
 		this.material = new THREE.MeshBasicMaterial({color: 0x0000ff, wireframe:false});
 		this.geometry = new THREE.CubeGeometry(2,2,2);
@@ -11,16 +17,57 @@ class Ship {
 
 		this.ship.add(this.mesh);
 		scene.add(this.ship);
+
+		this.left =0;
+		this.right=0;
+
+	}
+
+	stopLeft(){
+		this.left = 0;
+	}
+	stopRight(){
+		this.right = 0;
+	}
+	startRight(clockValue){
+		this.right = clockValue;
+	}
+
+	startLeft(clockValue){
+		this.left = clockValue;
 	}
 
 	setPosition(x,y,z){
 		this.x = x;
 		this.y = y;
 		this.z = z;
-		this.updatePosition();
 	}
 
-	updatePosition(){
+	timePassed(dt){
+		var positive = this.speed>0 ? 1:-1
+
+		//No key pressed: slow down
+		if((this.left + this.right) == 0){
+
+											//Stop if too slow o:r slow speed
+			this.speed = (Math.abs(this.speed) < MIN_SPEED) ? 0 : this.speed*0.95;
+
+		//Key pressed	: full throtle
+		} else {
+			var acelaration = ((this.left>this.right) ? -1:1)*ACELARATION
+			this.speed += acelaration*dt;
+
+			//Stop accelarating if max speed achieved
+			this.speed = (Math.abs(this.speed) > TOP_SPEED) ? TOP_SPEED * positive : this.speed;
+		}
+
+		if(DEBUG){	console.log("speed: " + this.speed); }
+		this.setPosition(this.x+this.speed*dt, this.y, this.z);
+	}
+
+	updatePosition(dt){
+		this.timePassed(dt);
+
 		this.ship.position.x = this.x;
 		this.ship.position.y = this.y;
 		this.ship.position.z = this.z;
