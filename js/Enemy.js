5cf7bc36a4b1b3c4fc55851503c8af1ae11ab17c
Miguel Amaral <miguel.amaral04@gmail.com>
2016-11-13 14:33:01 +0000

    colision aliens

--- js/Enemy.js
+++ js/Enemy.js
@@ -11,7 +11,7 @@ class Enemy extends Movable{
 		velocity.multiplyScalar(ALIEN_SPEED); // Initial speed
 		this.setSpeed(velocity.x, velocity.y, velocity.z);
 
-		// ------------------------------ ALIEN MATERIALS --------------------------------------- //			
+		// ------------------------------ ALIEN MATERIALS --------------------------------------- //
 		this.material = new THREE.MeshBasicMaterial( { color: cor } );
 		this.material.side = THREE.DoubleSide;
 
@@ -47,11 +47,11 @@ class Enemy extends Movable{
 		// this.positionElement(this.geometry, this.material, 9, 0, 2);
 		//this.geometry = new THREE.CubeGeometry(10, 10, 2);
 		//this.positionElement(this.geometry, this.material, 5, 0, 5);
-			//Legs
+		//Legs
 		var vec_cylinder = [];
 		for (var i = 0; i < 4; i++) {
 			var cylinder = new THREE.CylinderGeometry( 1, 1, 3, 5, 2, false);
-			cylinder.applyMatrix( new THREE.Matrix4().makeRotationX( THREE.Math.degToRad( 90 ) ) );		
+			cylinder.applyMatrix( new THREE.Matrix4().makeRotationX( THREE.Math.degToRad( 90 ) ) );
 			vec_cylinder.push(cylinder);
 		}
 		this.geometry = vec_cylinder[0];
@@ -62,12 +62,12 @@ class Enemy extends Movable{
 		this.positionElement(this.geometry, this.material, 7, 1, 4.5);
 		this.geometry = vec_cylinder[3];
 		this.positionElement(this.geometry, this.material, 7, -2, 4.5);
-			//Hat
-		var hat =  new THREE.CylinderGeometry( 2, 0.5, 2, 3, 1, false);	
-		hat.applyMatrix( new THREE.Matrix4().makeRotationX( THREE.Math.degToRad( 90 ) ) );	
+		//Hat
+		var hat =  new THREE.CylinderGeometry( 2, 0.5, 2, 3, 1, false);
+		hat.applyMatrix( new THREE.Matrix4().makeRotationX( THREE.Math.degToRad( 90 ) ) );
 		this.geometry = hat;
  		this.positionElement(this.geometry, this.material, 5, 0, -5)
- 			//eyes
+		//eyes
  		this.geometry = new THREE.SphereGeometry(1, 0, 0, Math.PI/2, Math.PI*2, 0, Math.PI);
 		this.positionElement(this.geometry, this.material, 3.5, 4, 0);
  		this.geometry = new THREE.SphereGeometry(1, 0, 0, Math.PI/2, Math.PI*2, 0, Math.PI);
@@ -88,10 +88,10 @@ class Enemy extends Movable{
 	}
 
 	getObjectCenter() {
-		return new THREE.Vector3(5,0,3.2);
+		return new THREE.Vector3(5,0,0);
 	}
 
 	getPowRadius() {
-		return 60.0625;
+		return 37;
 	}
 }
